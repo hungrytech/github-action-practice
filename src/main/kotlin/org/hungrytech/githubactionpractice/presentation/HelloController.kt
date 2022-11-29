bcb5cfafe0deb817371326c259a45ed2177967bb
hungrytech <xorals9448@gmail.com>
2022-11-29 22:50:32 +0900

    refactor: PR github action 테스트용 수정

--- src/main/kotlin/org/hungrytech/githubactionpractice/presentation/HelloController.kt
+++ src/main/kotlin/org/hungrytech/githubactionpractice/presentation/HelloController.kt
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
 
     @GetMapping("/hello")
-    fun hello() = "hello world TEST"
+    fun hello() = "hello world TEST PR"
 }
